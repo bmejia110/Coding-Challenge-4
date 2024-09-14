a1ccbb21f9558a5c7d86f87568263e8ce021ebaf
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-14 19:14:11 -0400

    create displayproductdetails function

--- inventory_management.js
+++ inventory_management.js
@@ -8,3 +8,16 @@ let inventory = [
     {name: "Drone", price: 800, quantity: 3, lowstocklevel: 1}
 ];
 // console.log(inventory);
+
+// Task 2 Create a Function to Display Product Details
+
+function displayProductDetails() {
+    let productstatus = product.quantity > product.lowstocklevel ? "in stock" : "low stock";
+    console.log("products: ", product.name);
+    console.log("Price: $", product.price);
+    console.log("Quantity in stock:", productquantity);
+    console.log("product status:", productstatus);
+}
+displayProductDetails(); 
+
+
