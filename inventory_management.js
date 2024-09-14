29af2220e65ed40bd0af7646c153c68e76074183
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-09-14 18:40:52 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,10 @@
+// Task 1 Create Inventory Array Of Product Objects
+
+let inventory = [
+    {name: "headphones", price: 150, quantity: 8, lowstocklevel: 3},
+    {name: "smartwatch", price: 250, quantity: 12, lowstocklevel: 2},
+    {name: "Gaming Console", price: 500, quantity: 6, lowstocklevel: 2},
+    {name: "Bluetooth Speaker", price: 100, quantity: 13, lowstocklevel: 5},
+    {name: "Drone", price: 800, quantity: 3, lowstocklevel: 1}
+];
+// console.log(inventory);
